d94d2af71c45051084c650191b74b7c0a8ddf014
Lipi C.H. Lee <lipisoft@gmail.com>
2017-11-13 09:32:33 +0900

    change to blocking mode for better performance

--- app/src/main/java/com/lipisoft/toyshark/ToySharkService.kt
+++ app/src/main/java/com/lipisoft/toyshark/ToySharkService.kt
@@ -3,21 +3,30 @@ package com.lipisoft.toyshark
 import android.app.Service
 import android.content.Intent
 import android.net.VpnService
+import android.os.Build
 import android.os.ParcelFileDescriptor
 import java.io.FileInputStream
 
-class ToysharkService : VpnService() {
+class ToySharkService : VpnService() {
     lateinit var vpnInterface : ParcelFileDescriptor
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         bringUpInterface()
 
         Thread(ReadInterface(vpnInterface)).start()
+//        Thread(WriteInterface(vpnInterface)).start()
 
         return Service.START_STICKY
     }
 
     private fun bringUpInterface() {
-        vpnInterface =  Builder().establish()
+        val builder =  Builder()
+                .addAddress("192.168.0.1", 32)
+                .addRoute("0.0.0.0", 0)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            builder.setBlocking(true)
+
+        vpnInterface = builder.establish()
     }
 
     override fun onRevoke() {
